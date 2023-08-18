e623b1658bf2a04b84b3250082f49886247ff900
maryanibrahim <maryanibrahim301@yahoo.com>
2023-08-18 00:22:59 +0000

    Made changes related to the db

---
+++ db/seeds/03_choices.sql
@@ -0,0 +1,7 @@
+-- Inserting choices for Alice's poll
+INSERT INTO choices (poll_id, title, description) VALUES (1, 'Choice A for Alice', 'This is a description for Choice A');
+INSERT INTO choices (poll_id, title) VALUES (1, 'Choice B for Alice');
+
+-- Inserting choices for Kira's poll
+INSERT INTO choices (poll_id, title, description) VALUES (2, 'Choice A for Kira', 'This is a description for Choice A');
+INSERT INTO choices (poll_id, title) VALUES (2, 'Choice B for Kira');
