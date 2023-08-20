069a70ed9b9e87251edd4e9da5e44fcf243e6c86
Sasha Andia <sasha.andia@gmail.com>
2023-08-19 19:36:37 -0700

    added a voter page and made a table of radio buttons

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use('/users', usersRoutes);
 // Separate them into separate routes files (see above).
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('voter');
 });
 
 app.listen(PORT, () => {
