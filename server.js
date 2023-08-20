6546ea14d8047e4bc9448718c41e09c2c94dc9bc
Sasha Andia <sasha.andia@gmail.com>
2023-08-20 09:42:21 -0700

    added a votes page that is accessible through /votes

--- server.js
+++ server.js
@@ -48,6 +48,10 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/votes', (req, res) => {
+  res.render('voter');
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
