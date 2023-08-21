ccf6b7c2987a223b96fc219dfccbdebd101ad677
Tolu Eshiet <tmeshiet@gmail.com>
2023-08-21 15:16:23 -0600

    "Corrected columns"

--- db/seeds/02_polls.sql
+++ db/seeds/02_polls.sql
@@ -1,5 +1,5 @@
 -- Inserting a sample poll created by Alice
-INSERT INTO polls (creator_email, admin_link, submission_link) VALUES ('alice@example.com', 'admin123', 'submit123');
+INSERT INTO polls (email, admin_link, submission_link) VALUES ('alice@example.com', 'admin123', 'submit123');
 
 -- Inserting a sample poll created by Kira
 INSERT INTO polls (creator_email, admin_link, submission_link) VALUES ('kira@example.com', 'admin456', 'submit456');
