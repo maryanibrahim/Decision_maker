1cabdd468c59a30b2ec4330b9c25bdb186f40571
Victoria Amirov <vika.amir20@gmail.com>
2023-08-21 10:35:30 -0700

    fixed polls.js

--- routes/users.js
+++ routes/users.js
@@ -43,35 +43,4 @@ router.post("/", (req, res) => {
 });
 
 
-// VIKA ADJUSTMENT
-router.get("/polls/:id", (req, res) => {
-  const pageID = req.params.id;
-
-  // If given ID is admin
-  if(pageID.startsWith('admin_')){
-    //check database if adminID exist
-    // if it exist put it in a variable
-    let databaseObject;
-
-    const templateVars = {
-      email: databaseObject.email,
-      name: databaseObject.name,
-      question_title: databaseObject.question_title,
-      choices: databaseObject.choices
-    }
-    // Render the admin page ejs file with templateVars
-    res.render("admin.ejs", templateVars);
-  } else {  // Given id is submissionID
-    //check database if submissionID exist
-    // if it exist put it in a variable
-    let databaseObject;
-
-    const templateVars = {
-      question_title: databaseObject.question_title,
-      choices: databaseObject.choices
-    }
-    // Render the submission page ejs file with templateVars
-    res.render("voter.ejs", templateVars);
-  }
-});
 
