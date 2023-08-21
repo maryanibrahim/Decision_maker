01ba710f45405ba4e57ad90cd887bef2160724bb
Victoria Amirov <vika.amir20@gmail.com>
2023-08-21 08:29:29 -0700

    added id to admin and get req to polls.id

--- routes/polls.js
+++ routes/polls.js
@@ -8,16 +8,15 @@ function generateRandomUserId() {
   return Math.floor(Math.random() * 100000);
 }
 
-
 router.get('/', async (req, res) => {
   try {
 
-    const title = req.body.question_title; // Get the title from the request body (assumes needs to change based on html)
+    const title = req.body.title; // Get the title from the request body (assumes needs to change based on html)
 
     // Call the Poll's create method to create a new poll
     const newPoll = await Poll.create(title);
 
-    res.render('polls', { poll: newPoll });
+    res.render('index', { poll: newPoll });
   } catch (error) {
     console.error('Error creating poll:', error);
     res.status(500).send('An error occurred while creating the poll.');
