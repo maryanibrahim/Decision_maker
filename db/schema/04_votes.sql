c4cc4f6fd85abfaaeb2d583fa87e5eb979baba0e
Tolu Eshiet <tmeshiet@gmail.com>
2023-08-21 15:16:23 -0600

    "Corrected columns"

--- db/schema/04_votes.sql
+++ db/schema/04_votes.sql
@@ -2,6 +2,8 @@
 DROP TABLE IF EXISTS votes CASCADE;
 CREATE TABLE votes (
   id SERIAL PRIMARY KEY NOT NULL,
+  poll_id INTEGER REFERENCES polls(id),
   choice_id INTEGER REFERENCES choices(id),
+  voter_name VARCHAR(255) NOT NULL,
   rank INTEGER NOT NULL
 );
