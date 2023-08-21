5f10caf91ff098935228a7e80929b93eb4209a07
Tolu Eshiet <tmeshiet@gmail.com>
2023-08-21 15:54:49 -0600

    modifying table'

--- db/schema/04_votes.sql
+++ db/schema/04_votes.sql
@@ -1,12 +1,7 @@
 -- Drop and recreate Votes table
 DROP TABLE IF EXISTS votes CASCADE;
 CREATE TABLE votes (
-<<<<<<< HEAD
-  id SERIAL PRIMARY KEY NOT NULL,
-  poll_id INTEGER REFERENCES polls(id),
-=======
   poll_id SERIAL PRIMARY KEY NOT NULL,
->>>>>>> 409f49a4a026b1ada0798cb1b4597de4c9a86b83
   choice_id INTEGER REFERENCES choices(id),
   voter_name VARCHAR(255) NOT NULL,
   rank INTEGER NOT NULL
