d2a11fbb8e2300056c57dc241dbd83cf563ca896
Tolu Eshiet <tmeshiet@gmail.com>
2023-08-22 18:30:10 -0600

    Added votes.model

--- db/queries/pollModel.js
+++ db/queries/pollModel.js
@@ -41,47 +41,6 @@ const Poll = {
     }
   },
 
-  getAdminData: async (adminLink) => {
-    try {
-      // Query to retrieve admin data from the polls table
-      const query = `
-        SELECT * FROM polls
-        WHERE admin_link = $1;
-      `;
-      const values = [adminLink];
-
-      const { rows } = await db.query(query, values);
-
-      if (rows.length === 0) {
-        return null; // No matching admin data found
-      }
-
-      return rows[0];
-    } catch (error) {
-      throw error;
-    }
-  },
-  // Method to get submission data by submission link
-  getSubmissionData: async (submissionLink) => {
-    try {
-      // Query to retrieve submission data from the polls table
-      const query = `
-      SELECT title, submission_link FROM polls
-      WHERE submission_link = $1;
-    `;
-      const values = [submissionLink];
-
-      const { rows } = await db.query(query, values);
-
-      if (rows.length === 0) {
-        return null; // No matching submission data found
-      }
-
-      return rows[0];
-    } catch (error) {
-      throw error;
-    }
-  },
   // Method to update vote count for a choice in the votes table
   updateVoteForChoice: async (pollId, choiceId, voterName, rank) => {
     try {
