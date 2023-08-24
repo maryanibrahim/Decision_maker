edf590706f5caa37898e1499ae9c1627124f041a
Victoria Amirov <vika.amir20@gmail.com>
2023-08-24 14:48:57 -0700

    added helper.js file and send/email function

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,9 @@ const router  = express.Router();
 const Poll = require('../db/queries/pollModel');
 const User = require('../db/queries/userModel');
 const choices = require('../db/queries/choicesModel');
+const sendEmail = require('./helper')
+
+
 
 function generateRandomString() {
   // found the solution on stackOverFlow
@@ -42,9 +45,7 @@ router.post("/", (req, res) => {
     // Generate ID's for both admin page and normal page
     let newAdminID = generateRandomString();
     let newSubmissionID = generateRandomString();
-    /*
-    SEND EMAIL to newUser.email with the Admin and Submission Links
-    */
+
     // Create the user data in Database
     User.create(newUser.email, newUser.name)
     .then((createdUser) => {
@@ -57,6 +58,7 @@ router.post("/", (req, res) => {
         // Create the new Choices entry in Database using
         choices.create(pollID, newUser.option1, newUser.option1_description, newUser.option2, newUser.option2_description, newUser.option3, newUser.option3_description, newUser.option4, newUser.option4_description)
         .then((createdChoices) => {
+          sendEmail("Here is your Poll links!", newAdminID, newSubmissionID,newUser.email )
 
           return res.status(200).send("Your Poll created succesfully! Please check your email for Admin Link and Submission Link.");
         })
