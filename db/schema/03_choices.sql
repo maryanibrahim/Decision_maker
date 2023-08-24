afb76158908cd7d282febdd3da0b701541d2a72e
maryan ibrahim <maryanibrahim301@yahoo.com>
2023-08-24 01:29:44 +0000

    options with their descriptions

--- db/schema/03_choices.sql
+++ db/schema/03_choices.sql
@@ -1,8 +1,19 @@
 -- Drop and recreate Choices table
 DROP TABLE IF EXISTS choices CASCADE;
+
 CREATE TABLE choices (
   id SERIAL PRIMARY KEY NOT NULL,
   poll_id INTEGER REFERENCES polls(id),
-  title VARCHAR(255) NOT NULL,
-  description TEXT
+  
+  option1 VARCHAR(255),
+  option1_description TEXT,
+  
+  option2 VARCHAR(255),
+  option2_description TEXT,
+  
+  option3 VARCHAR(255),
+  option3_description TEXT,
+  
+  option4 VARCHAR(255),
+  option4_description TEXT
 );
