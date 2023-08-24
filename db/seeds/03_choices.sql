3fe3ad89a5abf547860ca4a771f91cbc5014ecc2
maryan ibrahim <maryanibrahim301@yahoo.com>
2023-08-24 01:31:09 +0000

    seeds for choices options

--- db/seeds/03_choices.sql
+++ db/seeds/03_choices.sql
@@ -1,7 +1,13 @@
--- Inserting choices for Alice's poll
-INSERT INTO choices (poll_id, title, description) VALUES (1, 'Choice A for Alice', 'This is a description for Choice A');
-INSERT INTO choices (poll_id, title) VALUES (1, 'Choice B for Alice');
+-- Inserting choices for Alice's poll with all options
+INSERT INTO choices (poll_id, option1, option1_description, option2, option2_description, 
+    option3, option3_description, option4, option4_description) 
+VALUES (1, 
+ 'Choice A for Alice', 'This is a description for Choice A', 'Choice B for Alice', 'Description for Choice B', 
+        'Choice C for Alice', 'Description for Choice C', 'Choice D for Alice', 'Description for Choice D');
 
--- Inserting choices for Kira's poll
-INSERT INTO choices (poll_id, title, description) VALUES (2, 'Choice A for Kira', 'This is a description for Choice A');
-INSERT INTO choices (poll_id, title) VALUES (2, 'Choice B for Kira');
+-- Inserting choices for Kira's poll with all options
+INSERT INTO choices (poll_id, option1, option1_description, option2, option2_description, 
+   option3, option3_description, option4, option4_description) 
+VALUES (2, 
+    'Choice A for Kira', 'This is a description for Choice A', 'Choice B for Kira', 'Description for Choice B', 
+    'Choice C for Kira', 'Description for Choice C', 'Choice D for Kira', 'Description for Choice D');
