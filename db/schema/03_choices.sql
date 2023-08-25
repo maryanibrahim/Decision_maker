c97e56aae9132a8819bb3a1481416c0a9d503640
Tolu Eshiet <tmeshiet@gmail.com>
2023-08-25 16:13:21 -0600

    Removed options descriptions

--- db/schema/03_choices.sql
+++ db/schema/03_choices.sql
@@ -4,16 +4,16 @@ DROP TABLE IF EXISTS choices CASCADE;
 CREATE TABLE choices (
   id SERIAL PRIMARY KEY NOT NULL,
   poll_id INTEGER REFERENCES polls(id),
-  
+
   option1 VARCHAR(255),
-  option1_description TEXT,
-  
+
+
   option2 VARCHAR(255),
-  option2_description TEXT,
-  
+
+
   option3 VARCHAR(255),
-  option3_description TEXT,
-  
+
+
   option4 VARCHAR(255),
-  option4_description TEXT
+
 );
