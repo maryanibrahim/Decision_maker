5e3ea9834fc14562f13aaa67f294269cc8accba4
Tolu Eshiet <tmeshiet@gmail.com>
2023-08-25 17:49:31 -0600

    Added sendSubmissionemail

--- routes/helper.js
+++ routes/helper.js
@@ -28,13 +28,43 @@ let sendEmail = function(subject, adminID, submissionID, email){
       console.log('Email sent: ' + info.response);
     }
   });
-
-
 }
+
+// Function to send submission email
+let sendSubmissionEmail = function(submissionLink, pollCreatorEmail) {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'vika.lhl20@gmail.com',
+      pass: 'rnnvcghsbbnqdxqg'
+    }
+  });
+
+  var mailContent = {
+    from: 'vika.lhl20@gmail.com',
+    to: pollCreatorEmail,
+    subject: 'Poll Submission Received',
+    text: `Hey, \n` +
+          `A submission has been received for your poll. Here is the submission link:\n` +
+          `Submission Link: http://localhost:8080/poll/votes/${submissionLink} \n` +
+          `Cheers`
+  };
+
+  transporter.sendMail(mailContent, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Submission Email sent: ' + info.response);
+    }
+  });
+};
+
+
 
 module.exports = {
-  sendEmail
-}
+  sendEmail,
+  sendSubmissionEmail
+};
 
 
 
