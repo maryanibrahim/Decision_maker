2cdbbca84c88129f31197467cab3e89e3fef9658
maryan ibrahim <maryanibrahim301@yahoo.com>
2023-08-25 21:29:42 +0000

    ranked options

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,28 @@
 const express = require("express");
-const router  = express.Router();
-const db = require('../db/connection');
-const Poll = require('../db/queries/pollModel')
+const router = express.Router();
+const Poll = require('../db/queries/pollModel');
 
+router.get("/:id", async (req, res) => {
+  try {
+    const pageID = req.params.id;
+    
+    // Retrieve poll information
+    const returnedPoll = await Poll.findAdminID(pageID);
+    
+    // Retrieve options with vote counts, ranked from best to worst
+    const optionsWithCounts = await Poll.getPollResults(pageID);
 
-router.get("/:id", (req, res) => {
-    Poll.findAdminID(pageID)
-    .then((returnedPoll) => {
+    const templateVars = {
+      adminID: returnedPoll.admin_link,
+      question_title: returnedPoll.title,
+      options: optionsWithCounts
+    };
 
-      // TO-DO: get all info ffrom DB for results
-
-      const templateVars = {
-        adminID: returnedPoll.admin_link,
-        question_title: returnedPoll.title
-      }
-      // Render the admin page ejs file with templateVars
-      res.render("polls", templateVars);
-    })
+    res.render("polls", templateVars);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
