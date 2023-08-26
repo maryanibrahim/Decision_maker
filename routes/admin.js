e6a33cd5b1d330f5ae54eb210af977ebb933e2d0
Victoria Amirov <vika.amir20@gmail.com>
2023-08-26 08:35:46 -0700

    changed back

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Poll = require('../db/queries/pollModel');
-const Choices = require("../db/queries/choicesModel");
 
 router.get("/:adminID", (req, res) => {
   try {
     const adminID = req.params.adminID;
-    if (!adminID) {
-      return res.status(400).send('Admin link is missing');
-    }
 
     // Retrieve poll information
     const returnedPoll = Poll.findAdminID(adminID);
@@ -28,21 +24,5 @@ router.get("/:adminID", (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-
-
-
-// router.post('/stats/:adminID', (req, res) => {
-//   try {
-//     const adminLink = req.params.adminID;
-//     const results =  Poll.getPollResults(Choices);
-
-//   }
-//   catch (error) {
-//     console.error("Error ", error);
-//     res.status(500).send("An error occurred.");
-//   }
-// })
-
-
 
 module.exports = router;
