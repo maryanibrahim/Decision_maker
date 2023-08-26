3dfb346d4facea1bc0115cc895f67576f261e866
maryan ibrahim <maryanibrahim301@yahoo.com>
2023-08-26 15:41:41 +0000

    Your commit message here

--- db/schema/03_choices.sql
+++ db/schema/03_choices.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const db = require('../connection');
 
 const Choices = {
@@ -63,3 +64,24 @@ const Choices = {
 };
 
 module.exports = Choices;
+=======
+-- Drop and recreate Choices table
+DROP TABLE IF EXISTS choices CASCADE;
+
+CREATE TABLE choices (
+  id SERIAL PRIMARY KEY NOT NULL,
+  poll_id INTEGER REFERENCES polls(id),
+  
+  option1 VARCHAR(255),
+  option1_description TEXT,
+  
+  option2 VARCHAR(255),
+  option2_description TEXT,
+  
+  option3 VARCHAR(255),
+  option3_description TEXT,
+  
+  option4 VARCHAR(255),
+  option4_description TEXT
+);
+>>>>>>> Stashed changes
