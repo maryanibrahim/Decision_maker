37e38b4429645b2315b7b5117050be8001c90c59
maryan ibrahim <maryanibrahim301@yahoo.com>
2023-08-26 15:40:54 +0000

    Your commit message here

--- db/schema/03_choices.sql
+++ db/schema/03_choices.sql
@@ -1,19 +1,65 @@
--- Drop and recreate Choices table
-DROP TABLE IF EXISTS choices CASCADE;
+const db = require('../connection');
 
-CREATE TABLE choices (
-  id SERIAL PRIMARY KEY NOT NULL,
-  poll_id INTEGER REFERENCES polls(id),
+const Choices = {
+  // Method to create a new choice entry
+  create: async (poll_id, option1, option1_description, option2, option2_description, option3, option3_description, option4, option4_description) => {
+    try {
+      const query = `
+        INSERT INTO Choices (id, poll_id, option1, option1_description, option2, option2_description, option3, option3_description, option4, option4_description)
+        VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8 ,$9)
+        RETURNING *;
+      `;
 
-  option1 VARCHAR(255),
-  option1_description TEXT,
+      const values = [poll_id, option1, option1_description, option2, option2_description, option3, option3_description, option4, option4_description];
 
-  option2 VARCHAR(255),
-  option2_description TEXT,
+      const { rows } = await db.query(query, values);
+      return {
+        ...rows[0], // Returning the newly created choice object
+      };
+    } catch (error) {
+      throw error;
+    }
+  },
 
-  option3 VARCHAR(255),
-  option3_description TEXT,
+  // Method to retrieve choices by poll_id
+  findByPollId: async (poll_id) => {
+    try {
+      const query = `
+        SELECT *
+        FROM choices
+        WHERE poll_id = $1;
+      `;
+      const values = [poll_id];
 
-  option4 VARCHAR(255),
-  option4_description TEXT
-);
+      const { rows } = await db.query(query, values);
+      return {
+        ...rows[0], // Returning the choices object related to the poll_id
+      };
+    } catch (error) {
+      throw error;
+    }
+  },
+  // Method to retrieve choices by poll_id and format them as an array
+  getChoicesForPoll: async (poll_id) => {
+    try {
+      const query = `
+        SELECT *
+        FROM choices
+        WHERE poll_id = $1;
+      `;
+      const values = [poll_id];
+
+      const { rows } = await db.query(query, values);
+      return [
+        rows[0].option1,
+        rows[0].option2,
+        rows[0].option3,
+        rows[0].option4
+      ];
+    } catch (error) {
+      throw error;
+    }
+  }
+};
+
+module.exports = Choices;
